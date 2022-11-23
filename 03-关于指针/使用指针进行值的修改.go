48e8d7ce68eabfe15254447c5adaeb5916b9e943
LinYH <867256260@qq.com>
2022-11-23 15:10:54 +0800

    字符串替换解释、形参与指针地址拓展

--- 03-关于指针/使用指针进行值的修改.go
+++ 03-关于指针/使用指针进行值的修改.go
@@ -14,6 +14,12 @@ func main() {
 	modifyFromPoint(&b)
 	fmt.Println("修改后b的值为：", b)
 	// 从结果可以得知，a的变量值未发生改变，b的变量值发生了改变
+
+	x, y := 1, 2
+	// 交换指针地址
+	swapPointAddr(&x, &y)
+	fmt.Println("交换指针地址后的值为：", x, y)
+	// 形参修改指针地址, 原参数的指针地址不变。
 }
 
 // 当前函数是未使用指针
@@ -27,4 +33,11 @@ func modifyFromPoint(num *int) {
 	// 使用指针地址所指向的修改值
 	*num = 100
 	fmt.Println("modifyFromPoint方法: ", *num)
+}
+
+func swapPointAddr(num, num2 *int) {
+	fmt.Println("swapPointAddr交换指针地址为：", num, num2)
+	// 将a,b对应的指针地址互换
+	num, num2 = num2, num
+	fmt.Println("swapPointAddr交换指针地址后的值为：", *num, *num2)
 }
